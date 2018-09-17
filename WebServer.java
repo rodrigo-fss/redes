43b4a943561f6836a2ffcd3d4f82a9808e35cacf
Rodrigo Silva <rodrigo.f.ss@uol.com.br>
2018-09-17 00:02:29 -0300

    [Alterado] webservice

--- WebServer.java
+++ WebServer.java
@@ -23,7 +23,7 @@ public final class WebServer
             thread.start();
         }
     }
-    final class HttpRequest implements Runnable
+    final static class HttpRequest implements Runnable
     {
         final static String CRLF = "\r\n";
         Socket socket;
@@ -41,16 +41,99 @@ public final class WebServer
                 System.out.println(e);
             }
         }
+
         private void processRequest() throws Exception
         {
-            /* Get a reference to the socket's input and output streams.
-            InputStream is = ?;
-            DataOutputStream os = ?;
+            // Get a reference to the socket's input and output streams.
+            InputStream is = socket.getInputStream();
+            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
             // Set up input stream filters.
-            ?
-            BufferedReader br = ?;
-            . . .*/
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+            // Get the request line of the HTTP request message.
+            String requestLine = br.readLine();
+            // Display the request line.
+            System.out.println();
+            System.out.println(requestLine);
+
+            // Get and display the header lines.
+            String headerLine = null;
+            while ((headerLine = br.readLine()).length() != 0) {
+                System.out.println(headerLine);
+            }
+
+            // Extract the filename from the request line.
+            StringTokenizer tokens = new StringTokenizer(requestLine);
+            tokens.nextToken(); // skip over the method, which should be "GET"
+            String fileName = tokens.nextToken();
+            // Prepend a "." so that file request is within the current directory.
+            fileName = "." + fileName;
+
+            // Open the requested file.
+            FileInputStream fis = null;
+            boolean fileExists = true;
+            try {
+                fis = new FileInputStream(fileName);
+            } catch (FileNotFoundException e) {
+                fileExists = false;
+            }
+
+            // Construct the response message.
+            String statusLine = null;
+            String contentTypeLine = null;
+            String entityBody = null;
+
+            if (fileExists) {
+                statusLine = "HTTP/1.0 200 OK" + CRLF;
+                contentTypeLine = "Content-type: text/html" +
+                        contentType( fileName ) + CRLF;
+            } else {
+                statusLine = "HTTP/1.0 404 Not Found" + CRLF;
+                contentTypeLine = "Content-type: text/html";
+                entityBody = "<HTML>" +
+                        "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                        "<BODY>Not Found</BODY></HTML>";
+            }
+
+            // Send the status line.
+            os.writeBytes(statusLine);
+            // Send the content type line.
+            os.writeBytes(contentTypeLine);
+            // Send a blank line to indicate the end of the header lines.
+            os.writeBytes(CRLF);
+
+            // Send the entity body.
+            if (fileExists) {
+                sendBytes(fis, os);
+                fis.close();
+            } else {
+                os.writeBytes(entityBody);
+            }
+
+
+            // Close streams and socket.
+            os.close();
+            br.close();
+            socket.close();
+        }
+
+        private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception
+        {
+            // Construct a 1K buffer to hold bytes on their way to the socket.
+            byte[] buffer = new byte[1024];
+            int bytes = 0;
+            // Copy requested file into the socket's output stream.
+            while((bytes = fis.read(buffer)) != -1 ) {
+                os.write(buffer, 0, bytes);
+            }
+        }
+
+        private static String contentType(String fileName)
+        {
+            if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+                return "text/html";
+            }
+            return "application/octet-stream";
         }
 
     }
